12388ac860b534a0c2c675c30c7fa85fdb99b0cd
Mohammed Rois <id.roismohammed@gmail.com>
2023-05-27 20:57:58 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <Fragment>
-                <nav className="p-2 shadow-sm navbar navbar-expand-lg fixed-top" style={{backgroundColor:'#20232F'}}>
+                <nav className=" shadow-sm navbar navbar-expand-lg fixed-top" id="header" style={{backgroundColor:'#20232F'}}>
                     <div className="container-fluid">
                         <a className="text-white judul-header navbar-brand" href="/">
                             Portfolio
@@ -21,33 +21,33 @@ class Header extends Component {
                             <span className="navbar-toggler-icon" />
                         </button>
                         <div className=" item-header collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className=" gap-4 navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">
+                            <ul className="gap-4 navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className=" nav-item">
+                                    <a className="text-white nav-link active" aria-current="page" href="#">
                                         Home
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#about">
+                                    <a className="text-white nav-link" href="#about">
                                         About
                                     </a>
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a
-                                        className="nav-link "
+                                        className="text-white nav-link "
                                         href="#services"
                                     >
                                         Services
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#portfolio">
+                                    <a className="text-white nav-link" href="#portfolio">
                                         Portfolio
                                     </a>
                                 </li>
 
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#contact">
+                                    <a className="text-white nav-link" href="#contact">
                                         Contact
                                     </a>
                                 </li>
