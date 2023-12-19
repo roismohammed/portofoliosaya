127f4d01ea851c1ad650e63ef59e05239e47851d
Mohammed Rois <id.roismohammed@gmail.com>
2023-12-20 00:42:37 +0700

    f

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
     })
     return (
         <Fragment>
-            <nav className={header ? 'stiky-header navbar fixed-top navbar-expand-lg bg-body-tertiary navbar-dark p-3 shadow-sm' : 'navbar fixed-top navbar-expand-lg  p-4 bg-body-tertiary navbar-dark '}>
+            <nav className={header ? 'stiky-header navbar fixed-top navbar-expand-lg bg-body-tertiary navbar-dark p-3 shadow-sm' : 'navbar fixed-top navbar-expand-lg  p-3 bg-body-tertiary navbar-dark '}>
                 <div className="container">
                     <a className="navbar-brand judul-header mx-md-5 " href="#" style={{ color: '#02E7FD',fontSize:'25px' }}>
                         Roess
