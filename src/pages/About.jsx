768de733b2a599096352288adbdb95de115984bd
Mohammed Rois <id.roismohammed@gmail.com>
2023-12-20 14:39:49 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,13 +10,14 @@ class About extends Component {
                     <div className="row">
                         <div className="col-md-4">
                             <img
+                                width={'600'}
                                 data-aos="fade-right"
                                 data-aos-duration="1500"
-                                className="about-foto pt-5"
-                                src="./rois.png" alt="" />
+                                className="about-foto mt-5"
+                                src="./roes.png" alt="" />
                         </div>
 
-                        <div className="col-md-7 p-5 pt-5">
+                        <div className="col-md-7 p-5 pt-4">
                             <div className="text-about text-white">
                                 <h1
                                     data-aos="fade-down"
@@ -28,7 +29,7 @@ class About extends Component {
                                     className="text">Frontend Developer!</h4>
                                 <p data-aos="fade-left"
                                     data-aos-duration="1500" >
-                                  Halo, nama saya Muhammad Rois. Saya seorang Junior Front-End Programmer dengan keahlian dalam pengembangan menggunakan bahasa pemrograman seperti JavaScript, Bootstrap, dan ReactJS. Saya juga memiliki pengalaman dalam merancang dan mengimplementasikan basis data, serta mengintegrasikan sistem dengan layanan pihak ketiga seperti API dan platform lainnya.
+                                    Halo, nama saya Muhammad Rois. Saya seorang Junior Front-End Programmer dengan keahlian dalam pengembangan menggunakan bahasa pemrograman seperti JavaScript, Bootstrap, dan ReactJS. Saya juga memiliki pengalaman dalam merancang dan mengimplementasikan basis data, serta mengintegrasikan sistem dengan layanan pihak ketiga seperti API dan platform lainnya.
                                 </p>
                             </div>
 
